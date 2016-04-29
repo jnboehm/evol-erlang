b3c6e55cfbed6e1a005803eb822e46d6eb387ddd
Jan Niklas Boehm <mail@jnboehm.com>
2016-04-29 16:55:55 +0200

    Add evol file with _very_ basic outline

---
+++ impl-tsp/src/evol.erl
@@ -0,0 +1,14 @@
+-module(evol).
+-export([]).
+
+mate(_Creature,_Creature) ->
+    {ok,mated}.
+
+mutate(_Creature) ->
+    ok. % random
+
+# Soll Wert in fitnessliste abspeichern
+fitness(_Creature) ->
+    ok.
+
+# getfittest() -> ok.
