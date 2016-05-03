be6254653f649b8d1395d0f133645bddd3d1928a
Jens Nazarenus <me@jens-na.de>
2016-05-03 14:03:35 +0200

    Fix comments in evol.erl

--- impl-tsp/src/evol.erl
+++ impl-tsp/src/evol.erl
@@ -7,8 +7,7 @@ mate(_Creature,_Creature) ->
 mutate(_Creature) ->
     ok. % random
 
-# Soll Wert in fitnessliste abspeichern
+% Soll Wert in fitnessliste abspeichern
 fitness(_Creature) ->
     ok.
-
-# getfittest() -> ok.
+% getfittest() -> ok.
