d7cf491b70d1840c2df653ea31d712068455733f
Jens Nazarenus <me@jens-na.de>
2016-05-12 17:30:16 +0200

    Add documentation

--- impl-tsp/src/parse_tsp_file.erl
+++ impl-tsp/src/parse_tsp_file.erl
@@ -1,14 +1,24 @@
--module(parse_tsp_file).
--compile(export_all).
+%%
+%% impl-tsp – Traveling Salesman Problem in Erlang
+%% @author Jan Niklas Böhm <mail@jnboehm.com>
+%% @author Jens Nazarenus <me@jens-na.de>
+%%
 
+-module(parse_tsp_file).
+-export([make_atsp_graph/1]).
+
+%% @doc Returns a pair of options and graph for the specified file.
+%% @spec make_atsp_graph(Filename::string()) -> tuple()
 make_atsp_graph(Filename) ->
   {ok, Filedesc} = file:open(Filename, [read]),
   Opts = parse_opts(Filedesc),
   {Opts, parse_graph(Filedesc, orddict:fetch(dimension, Opts))}.
 
-%% Returns an orddict filled with the options
+%% @doc Entry point for the function parse_opts/2
 parse_opts(Filedesc) ->
   parse_opts(Filedesc, orddict:new()).
+
+%% @doc Returns an orddict filled with the options
 parse_opts(Filedesc, Options) ->
   {ok, Line} = file:read_line(Filedesc),
   UpdatedOptions = case string:tokens(Line, ":") of
@@ -29,13 +39,13 @@ parse_opts(Filedesc, Options) ->
     false -> parse_opts(Filedesc, UpdatedOptions)
   end.
 
-trim_str(Str) ->
-  string:strip(string:strip(Str, both, $\n)).
 
+%% @doc Entry point for function set_up_vertices/2
 set_up_vertices(Dim) when Dim > 0 ->
   set_up_vertices(digraph:new(), Dim).
 
-set_up_vertices(Graph, [Dim]) when is_integer(Dim) ->
+%% @doc Creates vertices according to the specified dimension.
+set_up_vertices(Graph, [Dim]) ->
   set_up_vertices(Graph, Dim);
 set_up_vertices(Graph, Dim) when Dim > 0->
   digraph:add_vertex(Graph, Dim, Dim),
@@ -43,11 +53,12 @@ set_up_vertices(Graph, Dim) when Dim > 0->
 set_up_vertices(Graph, 0) ->
   Graph.
 
-
+%% @doc Entry point for function parse_graph/5
 parse_graph(Filedesc, [Dim]) when Dim > 0 ->
   Graph = set_up_vertices(Dim),
   parse_graph(Filedesc, Dim, Graph, 1, 1).
 
+%% @doc Reads file number by number and adds edges to the given graph.
 parse_graph(_Filedesc, Dim, Graph, Row, _Col) when (Row > Dim) ->
   Graph;
 parse_graph(Filedesc, Dim, Graph, Row, Col) when (Row =< Dim) , (Col < Dim) ->
@@ -58,3 +69,7 @@ parse_graph(Filedesc, Dim, Graph, Row, Col) when (Col =:= Dim) , (Row =< Dim) ->
   {ok, [Num]} = io:fread(Filedesc, "", " ~d"),
   digraph:add_edge(Graph, Row, Col, Num),
   parse_graph(Filedesc, Dim, Graph, Row + 1, 1).
+
+%% @doc Trims spaces and new line characters.
+trim_str(Str) ->
+  string:strip(string:strip(Str, both, $\n)).
