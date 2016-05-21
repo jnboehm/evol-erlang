df73c9381b0e070b49efe87668f304863cfe9e27
Jens Nazarenus <me@jens-na.de>
2016-05-21 13:04:54 +0200

    Create random roundtrips

--- impl-tsp/src/evol.erl
+++ impl-tsp/src/evol.erl
@@ -8,7 +8,24 @@
 -export([init/0]).
 -compile(export_all).
 
-init() -> 
-  {Opts, Graph} = parse_tsp_file:make_atsp_graph("../data/br17.atsp"),
-  T = [digraph:edge(Graph, Edge) || Edge <- digraph:edges(Graph)].
+%% @doc Entry point for get_rnd_roundtrip/3
+get_rnd_roundtrip(Vertices, N) ->
+  get_rnd_roundtrip(Vertices, N, []).
 
+%% @doc Creates N random roundtrips.
+get_rnd_roundtrip(_Vertices, 0, VertexList) ->
+  VertexList;
+
+get_rnd_roundtrip(Vertices, N, VertexList) ->
+  Trip = [X || {_,X} <- lists:sort([{random:uniform(), Vertex} || Vertex <- Vertices])],
+  get_rnd_roundtrip(Vertices, N-1, [Trip|VertexList]).
+
+init(InitialRoundtrips, FileName) ->
+  {Opts, Graph} = parse_tsp_file:make_atsp_graph(FileName),
+  EdgeList = [digraph:edge(Graph, Edge) || Edge <- digraph:edges(Graph)],
+
+  Roundtrips = get_rnd_roundtrip(digraph:vertices(Graph), InitialRoundtrips).
+
+%% @doc
+init() ->
+  init(10, "../data/br17.atsp").
