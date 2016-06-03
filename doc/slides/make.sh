790cf396eaa4c685375afed01b24eec0bd9fb3ab
Jens Nazarenus <me@jens-na.de>
2016-06-03 11:53:43 +0200

    Inline graphviz graphs

--- doc/slides/make.sh
+++ doc/slides/make.sh
@@ -1,4 +1,2 @@
 #/bin/bash
-
-for i in $(ls -1 graphs/*.dot); do dot2tex $i --figonly --autosize -ftikz > $(echo $i |sed 's/dot/tex/g'); done
-lualatex pres.tex
+latexmk -xelatex -shell-escape -pvc pres.tex  
