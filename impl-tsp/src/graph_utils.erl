e9298c60dc79c572793b7a9e2369222b9f3f745e
Jens Nazarenus <me@jens-na.de>
2016-07-04 18:14:15 +0200

    3-opt move implementation nagata

--- impl-tsp/src/graph_utils.erl
+++ impl-tsp/src/graph_utils.erl
@@ -20,6 +20,13 @@ get_weight(EdgeList, V1, V2) ->
     [] -> undef
   end.
 
+get_weight(EdgeList, E) ->
+  F = fun({E1, _, _, _W}) -> E1 =:= E end,
+  case lists:filter(F, EdgeList) of
+    [{_,_,_,W}] -> W;
+    [] -> undef
+  end.
+
 %% @doc Creates an edge list for the given graph.
 get_edge_list(Graph) ->
   [digraph:edge(Graph, Edge) || Edge <- digraph:edges(Graph)].
@@ -32,6 +39,14 @@ get_fitness(EdgeList, Roundtrip) ->
               || N <- seq(1, length(Roundtrip))],
   sum(Weights).
 
+%% @doc Returns the fitness value for the individual (roundtrip)
+%%
+%% EdgeList - the list where the weights can be found
+%% G - the graph which represents the roundtrip
+get_fitness_graph(EdgeList, G) ->
+  Weights = [ get_weight(EdgeList, X) || X <- digraph:edges(G) ],
+  sum(Weights).
+
 %% Returns true iff the given vertex has a degree of 4. Returns false
 %% otherwise.
 %% G - the graph
@@ -40,7 +55,7 @@ has_deg_4(G, V) ->
   length(get_unique_neighbors(G, V)) =:= 4.
 
 
-%% Creates a ghost node for the given Node V in the given union graph.
+%% Creates a ghost node for the given Node V in the given graph.
 %% Note: Ghost nodes can only be created if the degree of the node is
 %% exactly 4.
 %%
@@ -63,7 +78,7 @@ create_ghost_node(GU, EdgeList, V) ->
   [{_,_,VO,WO}, {_,_,V1,W1}] = lists:filter(fun({E, _V1, _V2, _W}) -> lists:member(E, Out) end, EdgeList),
     
   digraph:add_edge(GU, -V, V1, W1), % E3
-  digraph:add_edge(GU, VO, -V, WO), % E2
+  digraph:add_edge(GU, -V, VO, WO), % E2
 
   % delete the original paths to V
   digraph:del_edge(GU, nth(1, Out)),
@@ -146,8 +161,7 @@ list_to_graph(L, EdgeList) ->
 %% Returns a unique list of neighbors for a given vertex in the
 %% specified graph.
 get_unique_neighbors(G, V) ->
-  sets:to_list(sets:from_list(digraph:out_neighbours(G, V))) 
-      ++ sets:to_list(sets:from_list(digraph:in_neighbours(G,V))).
+  sets:to_list(sets:from_list(digraph:out_neighbours(G, V) ++ digraph:in_neighbours(G,V))).
 
 %% @doc Transforms a graph into a list.
 %% L - the list to transform into a graph.
@@ -175,8 +189,107 @@ optmove2(G,V1,V2,EdgeList) ->
   digraph:del_edge(G, hd(digraph:out_edges(G, V1))),
   digraph:del_edge(G, hd(digraph:out_edges(G, V2))).
 
-  % direction swap
+  % direction swap?
   % TODO
+
+%% Performs a 3-opt move iff it improves the roundtrip.
+%%
+%% For the three nodes, assuming that V1 > V3 > V5 is
+%% the order in the roundtrip. Further we assume that for each V in G
+%% exists axactly one outgoing neighbor.
+%%
+%% Before:
+%%
+%% ... -> [V1] -> [V2] -> [V3] -> [V4] -> [V5] -> [V6] -> ...
+%%
+%%
+%% After:
+%%
+%%         +-----------------------+
+%%         |                       |
+%%         |                       V
+%% ... -> [V1]    [V2] -> [V3]    [V4] -> [V5]    [V6] -> ...
+%%                 ^       |               |       ^
+%%                 |       |               |       |
+%%                 |       +-----------------------+
+%%                 |                       |
+%%                 +-----------------------+
+%%
+%% G - the graph
+%% V1 - the first vertex
+%% V3 - the second vertex
+%% V5 - the third vertex
+%%
+%% Returns true if the tour has been improved, return false otherwise.
+optmove3(G, V1, V3, V5, EdgeList) ->
+  V2 = hd(digraph:out_neighbours(G, V1)),
+  V4 = hd(digraph:out_neighbours(G, V3)),
+  V6 = hd(digraph:out_neighbours(G, V5)),
+
+  % weights before
+  W_V1_V2 = get_weight(EdgeList, V1, V2),
+  W_V3_V4 = get_weight(EdgeList, V3, V4),
+  W_V5_V6 = get_weight(EdgeList, V5, V6),
+
+  % weights after
+  W_V1_V4 = get_weight(EdgeList, V1, V4),
+  W_V3_V6 = get_weight(EdgeList, V3, V6),
+  W_V5_V2 = get_weight(EdgeList, V5, V2),
+  
+  case (W_V1_V4 + W_V3_V6 + W_V5_V2) < (W_V1_V2 + W_V3_V4 + W_V5_V6) of
+    true -> 
+      digraph:del_edge(G, hd(digraph:out_edges(G, V1))),
+      digraph:del_edge(G, hd(digraph:out_edges(G, V3))),
+      digraph:del_edge(G, hd(digraph:out_edges(G, V5))),
+      digraph:add_edge(G, V1, V4, W_V1_V4),
+      digraph:add_edge(G, V3, V6, W_V3_V6),
+      digraph:add_edge(G, V5, V2, W_V5_V2),
+      true;
+    false ->
+      false
+  end.
+
+%% @doc Implementation of the 3-opt-move algorithm as described in 2.3 of 
+%% "Y. Nagata and D. Soler. A new genetic algorithm for the asymmetric 
+%% TSP. Expert Syst. with Applications, 39(10):8947–8953, 2012."
+%%
+%% G - the graph (roundtrip) to perform the 3-opt-moves
+%% EdgeList - the edge list with all the weights
+optmove3_run(G, EdgeList) ->
+  BitList = [ {V, false} || V <- digraph:vertices(G) ],
+  optmove3_run(G, EdgeList, BitList, length(BitList)-1).
+
+optmove3_run(G, EdgeList, BitList, -1) ->
+  ok;
+optmove3_run(G, EdgeList, BitList, N) ->
+  ok. 
+
+%% @doc Returns the next valid 3-tupel of vertices which may be used to
+%% perform a 3-opt-move. This function respects the 3-opt-move variant
+%% where "don't look bits" are used.
+%%
+%% Bitlist - The 2 tuple of {Vertex, true|false}
+%% Neighborhood - the neighborhood for the specified V
+%% V - the vertex to start looking for a triple
+optmove3_next_triple(BitList, Neighborhood) ->
+  ok.
+
+%% @doc For a given V in G return the subneighborhood as described in
+%% "A new genetic algorithm for the asymmetric TSP"
+%% -> "V4 is restricted to the vertices that are at most within the
+%% *ten* nearest from V4"
+%%
+%% G - the graph
+%% V - the V to to satisfy condition N(V) = V1
+optmove3_get_subneighborhood(G, V) ->
+  optmove3_get_subneighborhood(G, V, [], 10).
+
+optmove3_get_subneighborhood(G, V, Neighborhood, 0) ->
+  lists:reverse(Neighborhood);
+
+optmove3_get_subneighborhood(G, V, Neighborhood, N) ->
+  X = hd(digraph:out_neighbours(G, V)),
+  optmove3_get_subneighborhood(G, X, [X|Neighborhood], N-1).
 
 %% @doc Displays a graph in a very unconventional way. :-)
 %% It uses fdp for graph rendering.
@@ -196,5 +309,3 @@ count(X, H) -> count(X, H, 0).
 count(_, [], Count) -> Count;
 count(X, [X|Rest], Count) -> count(X, Rest, Count+1);
 count(X, [_|Rest], Count) -> count(X, Rest, Count).
-
-
