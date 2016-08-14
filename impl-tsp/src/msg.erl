c59aab727edec71a8dafab9d382b84d7d90f7a8b
Jan Niklas Boehm <mail@jnboehm.com>
2016-08-14 20:52:10 +0200

    Change the msg module to call the evol_gapx:run/2

--- impl-tsp/src/msg.erl
+++ impl-tsp/src/msg.erl
@@ -10,30 +10,28 @@ master_spawn() ->
 %% Spawns all the processes and then returns the first and last pid of
 %% the created ones.  The processes each have the pid of the one they
 %% spawn and the last one gets the pid of the first process.
-master_spawn(Filename, ProcNum) when ProcNum > 0 andalso is_integer(ProcNum) ->
-  {Opts, Graph} = evol:init(10, Filename),
-  Firstpid = spawn(?MODULE, slave_spawn, [self(), Filename, Opts, Graph, ProcNum]),
+master_spawn(Graph, Opts) ->
+  Firstpid = spawn(?MODULE, slave_spawn, [self(), Opts, Graph, orddict:fetch(proc_num, Opts)]),
   receive
     {procs_spawned, Pid} -> Pid ! {firstpid, Firstpid},	% complete the circle
-			    {Firstpid, Pid}
+                            {Firstpid, Pid}
   end.
 
-slave_spawn(MasterPid, _Filename, _Opts, _Graph, 0) ->
+slave_spawn(MasterPid, Opts, Graph, 0) ->
   MasterPid ! {procs_spawned, self()},
   receive
     {firstpid, Pid} -> ok
   end,
   %% Pid needs to be passed to the evol module in order to send the
   %% roundtrips around.
-  get_msg(Pid, _Graph);
-slave_spawn(MasterPid, Filename, Opts, Graph, ProcNum) ->
-  Pid = spawn(?MODULE, slave_spawn, [MasterPid, Filename, Opts, Graph, ProcNum - 1]),
+  evol_gapx:run(Graph, orddict:store(pid, Pid, Opts));
+slave_spawn(MasterPid, Opts, Graph, ProcNum) ->
+  Pid = spawn(?MODULE, slave_spawn, [MasterPid, Opts, Graph, ProcNum - 1]),
   %% We actually need to call the runner function, which still needs to be written.
-  %% evol:init(10, Filename)
-  get_msg(Pid, Graph).
+  evol_gapx:run(Graph, orddict:store(pid, Pid, Opts)).
 
 get_msg(Pid, PNum) ->
-  Pid ! {self(), digraph:no_edges(PNum)},
+  Pid ! {self(), PNum},
   receive
     {From, Msg} ->
       io:format("~p got a msg from ~p: ~p~n", [self(), From, Msg])
