58303b24b0140bb2638c46223f9511553e2db44d
Jan Niklas Boehm <mail@jnboehm.com>
2016-08-15 01:20:48 +0200

    Recurse everytime in ets_gc:handle/1
    forgot that in a couple branches.  Also erase the key form the dictif we
    delete the graph.

--- impl-tsp/src/ets_gc.erl
+++ impl-tsp/src/ets_gc.erl
@@ -9,19 +9,23 @@ init() ->
 handle(GraphDict) ->
   receive
     {del, Graph} ->
-      %% lists:filter(fun(G) -> G =:= Graph end, GraphList),
       G = dict:find(Graph, GraphDict),
       case G of
-        error -> digraph:delete(Graph);           % noone uses this graph
-        {ok, Val} -> if Val > 1 -> handle(dict:update_counter(Graph, -1, GraphDict));
-                        Val =< 1  -> digraph:delete(Graph)
+        error -> digraph:delete(Graph),
+                 NewD = GraphDict;           % noone uses this graph
+        {ok, Val} -> if Val > 1 -> NewD = dict:update_counter(Graph, -1, GraphDict);
+                        Val =< 1  -> digraph:delete(Graph),
+                                     NewD = dict:erase(Graph, GraphDict)
                      end
-      end;
-    {use, Graph} ->       G = dict:find(Graph, GraphDict),
+      end,
+      handle(NewD);
+    {use, Graph} ->
+      G = dict:find(Graph, GraphDict),
       case G of
-        error -> handle(dict:store(Graph, 1, GraphDict));
-        {ok, _} -> handle(dict:update_counter(Graph, 1, GraphDict))
-      end;
+        error -> NewD = dict:store(Graph, 1, GraphDict);
+        {ok, _} ->  NewD = dict:update_counter(Graph, 1, GraphDict)
+      end,
+      handle(NewD);
     Unexpected -> io:format("WTF is this hist: ~p?~n", [Unexpected]),
                   error(wrong_msg_format)
   end.
