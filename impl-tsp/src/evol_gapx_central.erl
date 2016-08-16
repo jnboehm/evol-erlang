cec26f9f7e8e2110c2f53a39777566a7f10ef408
Jan Niklas Boehm <mail@jnboehm.com>
2016-08-16 09:54:17 +0200

    Stuff last mutation into the dict

--- impl-tsp/src/evol_gapx_central.erl
+++ impl-tsp/src/evol_gapx_central.erl
@@ -7,7 +7,7 @@ init(FileName, Nodes, PopSize, ProcessNum, NSize) ->
   optmove3:init_nif(),
   {GraphOpts, Graph} = parse_tsp_file:make_atsp_graph(FileName),
   OptList = [{pop_size, PopSize}, {neigh_size, NSize},
-             {proc_num, ProcessNum}],
+             {proc_num, ProcessNum}, {last_mut, 0}],
   Opts = orddict:merge(fun(_,_,_) -> ok end, GraphOpts, orddict:from_list(OptList)),
   PidL = spawn_slaves(ProcessNum),
   lists:foreach(fun(Node) -> net_adm:ping(Node) end, Nodes),
@@ -32,7 +32,7 @@ master_proc(Graph, Opts, Pids) ->
   F = fun(Pid) -> Pid ! {make_offspring, self(),
                          {Graph, InitPop, orddict:fetch(neigh_size, Opts)}} end,
   lists:foreach(F, Pids),
-  master_loop(Graph, Opts, Pids, length(Pids), 1, 0, InitPop, []).
+  master_loop(Graph, Opts, Pids, length(Pids), 1, InitPop, []).
 
 %% @doc the workers.  They create a roundtrip and send it back to the
 %% master process.  The tables which the graph is made of have to have
@@ -48,21 +48,23 @@ slave_handle() ->
     stop -> ok
   end.
 
-master_loop(Graph, Opts, Pids, 0, Gen, LastMut, [{_, PopF} | _] = Pop, Offsprings) ->
+master_loop(Graph, Opts, Pids, 0, Gen, [{_, PopF} | _] = Pop, Offsprings) ->
+  LastMut = orddict:fetch(last_mut, Opts),
   NewPop = evol_gapx:update_population(Pop, Offsprings, length(Pop)),
   lists:foreach(fun(Node) -> {G, _} = hd(NewPop), L = graph_utils:roundtrip_to_list(G),
                              {evol_master, Node} ! {other_node, L} end, nodes()),
   lists:foreach(fun(Pid) -> Pid ! {make_offspring, self(),
                              {Graph, NewPop, orddict:fetch(neigh_size, Opts)}} end, Pids),
-  if LastMut >= 20 -> NewOpts = orddict:update_counter(neigh_sizr, 1, Opts);
-     LastMut < 20 -> NewOpts = Opts
-  end,
-  {_, NewPopF} = hd(NewPop),
-  NewLM = if NewPopF < PopF -> 0;
-              NewPopF >= PopF -> LastMut + 1
-           end,
-  master_loop(Graph, NewOpts, Pids, length(Pids), Gen + 1, NewLM, NewPop, []);
-master_loop(Graph, Opts, Pids, N, Gen, LastMut, Pop, Offsprings) ->
+  {_, NewPopF} = hd(Offsprings),
+  NewOpts = if LastMut >= 20 -> 
+                orddict:update(last_mut, 0, orddict:update_counter(neigh_size, 1, Opts));
+               LastMut < 20 -> if NewPopF < PopF -> orddict:update(last_mut, 0, Opts);
+                                             NewPopF >= PopF -> orddict:update_counter(last_mut, 1, Opts)
+                               end;
+                        NewPopF >= PopF -> orddict:update_counter(last_mut, 1, Opts)
+            end,
+    master_loop(Graph, NewOpts, Pids, length(Pids), Gen + 1, NewPop, []);
+master_loop(Graph, Opts, Pids, N, Gen, Pop, Offsprings) ->
   receive
     {other_node, OList} ->
       NodeGraph = graph_utils:list_to_graph(OList, Graph),
@@ -71,27 +73,27 @@ master_loop(Graph, Opts, Pids, N, Gen, LastMut, Pop, Offsprings) ->
           case evol_gapx:verify_graph(Offsprings, NodeGraph) of
             unique ->
               O = {NodeGraph, graph_utils:get_fitness_graph(NodeGraph)},
-              master_loop(Graph, Opts, Pids, N, Gen, LastMut, Pop, [O | Offsprings]);
+              master_loop(Graph, Opts, Pids, N, Gen, Pop, [O | Offsprings]);
             duplicate -> digraph:delete(NodeGraph),
-                     master_loop(Graph, Opts, Pids, N, Gen, LastMut, Pop, Offsprings)
+                     master_loop(Graph, Opts, Pids, N, Gen, Pop, Offsprings)
           end;
         duplicate -> digraph:delete(NodeGraph),
-                     master_loop(Graph, Opts, Pids, N, Gen, LastMut, Pop, Offsprings)
+                     master_loop(Graph, Opts, Pids, N, Gen, Pop, Offsprings)
       end;
     {offspring, {G, _} = O} ->
       case evol_gapx:verify_graph(Offsprings, O) of
-        unique -> master_loop(Graph, Opts, Pids, N - 1, Gen, LastMut, Pop, [O | Offsprings]);
+        unique -> master_loop(Graph, Opts, Pids, N - 1, Gen, Pop, [O | Offsprings]);
         duplicate -> digraph:delete(G),
-                     master_loop(Graph, Opts, Pids, N - 1, Gen, LastMut, Pop, Offsprings)
+                     master_loop(Graph, Opts, Pids, N - 1, Gen, Pop, Offsprings)
       end;
     {Pid, info} -> Pid ! {ok, {Gen, hd(Pop), lists:last(Pop)}},
-                   master_loop(Graph, Opts, Pids, N, Gen, LastMut, Pop, Offsprings);
+                   master_loop(Graph, Opts, Pids, N, Gen, Pop, Offsprings);
     {Pid, population} -> Pid ! {ok, Pop},
-                         master_loop(Graph, Opts, Pids, N, Gen, LastMut, Pop, Offsprings);
-    {Pid, misc} -> Pid ! {ok, Opts, Pids, Gen, LastMut},
-                   master_loop(Graph, Opts, Pids, N, Gen, LastMut, Pop, Offsprings);
+                         master_loop(Graph, Opts, Pids, N, Gen, Pop, Offsprings);
+    {Pid, misc} -> Pid ! {ok, Opts, Pids, Gen},
+                   master_loop(Graph, Opts, Pids, N, Gen, Pop, Offsprings);
     {'ETS-TRANSFER',_,_,ok} ->                  % do nothing
-      master_loop(Graph, Opts, Pids, N, Gen, LastMut, Pop, Offsprings);
+      master_loop(Graph, Opts, Pids, N, Gen, Pop, Offsprings);
     {Pid, stop} -> lists:foreach(fun(P) -> P ! stop end, Pids),
                    Pid ! {ok, {Gen, hd(Pop), lists:last(Pop)}}
   end.
