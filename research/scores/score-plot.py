c43c8d0a75776d5a26557113d575a35aa5cbdfde
Jan Niklas Boehm <mail@jnboehm.com>
2016-08-31 15:30:19 +0200

    Better plotting of fitness vals

--- research/scores/score-plot.py
+++ research/scores/score-plot.py
@@ -37,13 +37,14 @@ def make_plot(fname):
     data['cur_ratio'] = data.apply(lambda x: x['current'] / x['best'], axis=1)
     data['avg_ratio'] = data.apply(lambda x: x['avg'] / x['best'], axis=1)
     ax = plt.figure().add_subplot(1,1,1)
-    ax.scatter(data['gen'], data['cur_ratio'], c=(0.882352941,0,0.098039216),
-               marker='o', alpha=.75)
-    ax.scatter(data['gen'], data['avg_ratio'], c=(0.274509804,0.254901961,0.235294118),
-               marker='o', alpha=.75)
+    ax.plot(data['gen'], data['cur_ratio'], c=(0.882352941,0,0.098039216),
+               marker='.', markersize=10, alpha=.75)
+    ax.plot(data['gen'], data['avg_ratio'], c=(0.274509804,0.254901961,0.235294118),
+               marker='.', markersize=10, alpha=.75)
 
     plt.tight_layout(.5)
-    plt.xlim([-10,110])
+    plt.xlim([-5,105])
+    plt.ylim([1.0, 1.7])
     plt.savefig(fname)
 
 def make_all_plots():
@@ -51,3 +52,5 @@ def make_all_plots():
         make_plot(filename)
     for filename in glob.glob('*-???-?'):
         make_plot(filename)
+
+make_all_plots()
